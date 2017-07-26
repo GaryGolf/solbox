4fc341fe590b5a03785436faea62a2d94bcc5dd1
GaryGolf <garygolf@mail.ru>
2017-07-26 17:49:09 +0300

    remove unused files

--- src/actions/index.ts
+++
@@ -1,7 +0,0 @@
-import * as User from './user'
-
-interface Interface {
-    user: User.Interface
-}
-
-export {Interface, User}
--- src/actions/user.ts
+++
@@ -1,8 +0,0 @@
-
-import * as Actions from '../constants/actions'
-
-export interface Interface {
-    confirmCode(accessToken:string): Action
-}
-
-export const confirmCode = accessToken => ({type: Actions.CONFIRM_CODE, payload: accessToken})
