72a19ce65b75c6c3af37d545ce37b5dde19482f4
GaryGolf <garygolf@mail.ru>
2017-07-27 12:30:05 +0300

    install react native camera for android

--- android/app/src/main/java/com/solbox/MainApplication.java
+++ android/app/src/main/java/com/solbox/MainApplication.java
@@ -3,6 +3,7 @@ package com.solbox;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -23,7 +24,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(), new SvgPackage()
+          new MainReactPackage(),
+            new RCTCameraPackage(), new SvgPackage()
       );
     }
   };
