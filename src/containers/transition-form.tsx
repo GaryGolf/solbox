d69399121e3c0324d0bc5a44b072c450fee1328c
GaryGolf <garygolf@mail.ru>
2017-07-27 17:41:57 +0300

    add hidden input to transition form

--- src/containers/transition-form.tsx
+++ src/containers/transition-form.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
-import {StyleSheet, Text, View, TextInput, TouchableHighlight } from 'react-native'
+import {StyleSheet, Text, View, TouchableHighlight } from 'react-native'
 import { NavigationParams, NavigationStackScreenOptions } from 'react-navigation'
 
 import Check from '../components/svg/check'
 import Return from '../components/svg/return'
 import Signature from '../components/signature'
+import HiddenInput from '../components/hidden-input'
 
 interface Props {
     navigation?: NavigationParams
@@ -61,24 +62,15 @@ export default class TransitionForm extends React.Component <Props, State> {
         return  (
             <View style={styles.container}>
                 <View style={styles.form}>
-                    <Text style={styles.label}>Start time</Text>
-                    <TextInput
-                        style={!!time||focus=='time'?styles.input:styles.empty}
-                        keyboardType={'default'}
-                        maxLength={180}
+                    <HiddenInput
+                        title="Start time"
+                        value={time}
                         autoFocus={true}
-                        onFocus={()=>this.setState({focus:'time'})}
-                        value={this.state.time}
-                        onChange={e=>this.setState({time:e.nativeEvent.text})}
+                        onChangeText={text=>this.setState({time:text})}
                     />
-                    <Text style={styles.label}>Depot signature name</Text>
-                    <TextInput
-                        style={!!name||focus=='name'?styles.input:styles.empty}
-                        keyboardType={'default'}
-                        maxLength={180}
-                        onFocus={()=>this.setState({focus:'name'})}
-                        value={this.state.name}
-                        onChange={e=>this.setState({name:e.nativeEvent.text})}
+                     <HiddenInput
+                        title="Depot signature name"
+                        onChangeText={text=>this.setState({name:text})}
                     />
                     <Text style={styles.label}>Depot in signature</Text>
                 </View>
@@ -103,16 +95,6 @@ const styles = StyleSheet.create({
         fontWeight: '300',
         color: '#bbb',
         marginTop: 15
-    },
-    empty: {
-       height: 0,
-       color: '#000'
-    },
-    input: {
-        fontSize: 24,
-        fontWeight: '800',
-        color: '#512da7',
-        width: '100%'
     },
     send: {
         margin: 20
